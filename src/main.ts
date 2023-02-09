1b21b1e6261e1109f517e98dc2f94684e2f98165
xingyijun <xing_yijun@venusgroup.com.cn>
2023-02-09 15:35:35 +0800

    feat: button组件新增属性

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from 'vue';
 import App from './app.vue';
-// import UselessUI from '../dist/useless-ui.mjs';
-// import '../dist/entry.css';
+import UselessUI from '../dist/useless-ui.mjs';
+import '../dist/entry.css';
 
 const app = createApp(App);
-// app.use(UselessUI);
+app.use(UselessUI);
 app.mount('#app');
