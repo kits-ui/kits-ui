80e038351c081217fdc0bd8eed203e1d96c6dde7
qianshi <2687346406@qq.com>
2023-02-27 23:08:38 +0800

    bug: 修复预览大图组件

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from 'vue';
 import App from './app.vue';
-import uselessUI from '../dist/index.es.js';
-import '../dist/entry.css';
+// import uselessUI from '../dist/index.es.js';
+// import '../dist/entry.css';
 
 const app = createApp(App);
-app.use(uselessUI);
+// app.use(uselessUI);
 app.mount('#app');
