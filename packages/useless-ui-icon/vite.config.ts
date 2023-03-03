f58d8ff4bd9ec3ee0608767dc087d8ee26fc01d1
dyh_a <xinzon32177@163.com>
2023-03-03 16:06:57 +0800

    chore(icon): build config

--- packages/useless-ui-icon/vite.config.ts
+++ packages/useless-ui-icon/vite.config.ts
@@ -1,7 +1,10 @@
 import type { UserConfigExport } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 
 export default (): UserConfigExport => {
   return {
+    plugins: [vue(), vueJsx({})],
     build: {
       minify: true,
       emptyOutDir: true,
