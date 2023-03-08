2a5a2077aeec8467a344a41d277f56263a3bf0c5
xingyijun <xing_yijun@venusgroup.com.cn>
2023-03-08 10:14:37 +0800

    1111

--- packages/kits-ui-icons/src/arrowdown.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <svg id="icon-arrowdown" viewBox="0 0 1024 1024">
-    <path
-      d="M512 685.248L233.376 406.624l45.248-45.248L512 594.752l233.376-233.376 45.248 45.248z"
-    ></path>
-  </svg>
-</template>
--- packages/kits-ui-icons/src/arrowleft.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <svg id="icon-arrowleft" viewBox="0 0 1024 1024">
-    <path
-      d="M589.088 790.624L310.464 512l278.624-278.624 45.248 45.248L400.96 512l233.376 233.376z"
-    ></path>
-  </svg>
-</template>
