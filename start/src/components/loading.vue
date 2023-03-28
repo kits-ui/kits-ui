aad2c630508aa55d099ec6457ac579e753325825
dyh_a <xinzon32177@163.com>
2023-03-29 06:17:13 +0800

    feat(components/loading): props添加color选项

--- start/src/components/loading.vue
+++ start/src/components/loading.vue
@@ -47,8 +47,8 @@ const showFnLoading = () =>
     text: '函数调用的loading,点击任意地方关闭loading',
     closeOnClick: true,
     customClass: 'red',
-    background: 'rgba(255,255,255,0.2)',
-    color: 'rgb(43, 192, 172)',
+    background: 'rgba(255,255,255,0.9)',
+    color: 'black',
   });
 
 const countdown = ref(3);
