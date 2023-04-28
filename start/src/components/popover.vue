f0d5a9a3afb159454d90b44f76e5499fb0a22e15
xingyijun <xing_yijun@venusgroup.com.cn>
2023-04-28 17:02:45 +0800

    fix: 修改popover

--- start/src/components/popover.vue
+++ start/src/components/popover.vue
@@ -1,21 +1,35 @@
 <template>
-  <k-popover title="点我干嘛呢？" trigger="click" theme="light">
-    <template #reference>
-      <k-button type="insert">点我</k-button>
+  <k-popover title="点我干嘛呢？" trigger="click" position="right">
+    <template #content>
+      <k-calendar v-model="value" :first-day="1" @on-change="onChange"></k-calendar>
     </template>
+    <template #title>
+      <span>Title</span>
+    </template>
+    <k-button type="insert">点我</k-button>
   </k-popover>
   <k-popover title="摸我干嘛呢？" trigger="hover">
-    <template #reference>
-      <k-button type="insert">摸我</k-button>
+    <template #content>
+      <k-calendar v-model="value" :first-day="1" @on-change="onChange"></k-calendar>
     </template>
+    <template #title>
+      <span>Title</span>
+    </template>
+    <k-button type="insert">摸我</k-button>
   </k-popover>
-  <k-popover title="按住我干嘛呢？" trigger="longpress">
+  <!-- <k-popover title="按住我干嘛呢？" trigger="longpress">
     <template #reference>
       <k-button type="insert">按住我</k-button>
     </template>
-  </k-popover>
+  </k-popover> -->
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+const value = ref(new Date());
+const onChange = (val: { date: Date; type: string }) => {
+  console.log(val);
+};
+</script>
 
 <style scoped></style>
