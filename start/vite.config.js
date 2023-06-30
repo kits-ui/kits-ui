3df016795bdff6ec012af27b4e9fc8ba34576c4f
xingyijun <xing_yijun@venusgroup.com.cn>
2023-06-30 17:52:01 +0800

    fix: 新增devTools

--- start/vite.config.js
+++ start/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
-
+import VueDevTools from 'vite-plugin-vue-devtools';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [VueDevTools(), vue(), vueJsx()],
   esbuild: {
     jsxFactory: 'h',
     jsxFragment: 'Fragment',
