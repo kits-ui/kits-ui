951ee59cd2812bd1db17b796f44446d05fb1ce46
xingyijun <xing_yijun@venusgroup.com.cn>
2023-07-05 20:50:43 +0800

    feat: 新增抽屉

--- packages/kits-ui/src/dialog/index.vue
+++ packages/kits-ui/src/dialog/index.vue
@@ -1,32 +1,36 @@
 <template>
   <Teleport to="body">
-    <dialog ref="kDialog" class="k-dialog">
-      <div v-if="props.modelValue">
-        <div class="k-dialog-header">
-          <slot name="header">
-            <span>{{ props.title }}</span>
-            <span class="k-dialog-closeBtn" @click="close">
-              <close1 />
-            </span>
-          </slot>
+    <Transition name="bounce">
+      <dialog ref="kDialog" class="k-dialog">
+        <div class="content" style="height: props.height">
+          <div>
+            <div class="k-dialog-header">
+              <slot name="header">
+                <span>{{ props.title }}</span>
+                <span class="k-dialog-closeBtn" @click="close">
+                  <close1 />
+                </span>
+              </slot>
+            </div>
+            <div class="k-dialog-body">
+              <slot></slot>
+            </div>
+            <div class="k-dialog-footer">
+              <slot name="footer">
+                <!-- <k-button @click="close">关闭</k-button> -->
+              </slot>
+            </div>
+          </div>
         </div>
-        <div class="k-dialog-body">
-          <slot></slot>
-        </div>
-        <div class="k-dialog-footer">
-          <slot name="footer">
-            <!-- <k-button @click="close">关闭</k-button> -->
-          </slot>
-        </div>
-      </div>
-    </dialog>
+      </dialog>
+    </Transition>
   </Teleport>
 </template>
 
 <script setup lang="ts">
-import { ref, watch } from 'vue';
+import { onMounted, ref, watch } from 'vue';
 import { close1 } from '../icon/index';
-console.log(6666666);
+import { setStyle } from '../utils/index';
 
 const props = defineProps({
   modelValue: {
@@ -37,26 +41,51 @@ const props = defineProps({
     type: String,
     default: '',
   },
+  height: {
+    type: String,
+    default: '50px',
+  },
 });
 
 const emit = defineEmits(['update:modelValue']);
 
-const kDialog = ref<any>();
+const kDialog = ref<HTMLDialogElement>();
+
+onMounted(() => {
+  // kDialog.value.addEventListener('transitionend', function (e) {
+  //   kDialog.value.close();
+  // });
+});
 
 watch(
   () => props.modelValue,
-  (newValue) => {
+  async (newValue) => {
     if (newValue) {
       kDialog.value.showModal();
+      setStyle(kDialog.value, {
+        visibility: 'visible',
+        transform: 'translateY(0%)',
+        opacity: '1',
+      });
     } else {
-      kDialog.value.close();
+      kDialog.value.style.transform = 'translateY(-40%)';
+      kDialog.value.style.opacity = '0';
+      kDialog.value.style.visibility = 'hidden';
+      // domClose();
+      setTimeout(() => {
+        kDialog.value.close();
+      }, 200);
     }
   },
 );
 
 const close = () => {
-  kDialog.value.close();
+  const res = kDialog.value.getBoundingClientRect();
+  console.log(res);
   emit('update:modelValue', false);
+  setTimeout(() => {
+    kDialog.value.close();
+  }, 200);
 };
 </script>
 
